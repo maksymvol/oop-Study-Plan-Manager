640e8107c699b738c80d388c68630076352cbb53
Maksym Volkov <maksvolkov26p2@gmail.com>
2019-03-06 21:41:06 +0200

    education plan applies to the expired date

--- src/educationPlanManager/EducationPlan.java
+++ src/educationPlanManager/EducationPlan.java
@@ -1,17 +1,23 @@
 package educationPlanManager;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class EducationPlan {
     private ArrayList<Activity> activities;
+    private LocalDate expirationDate;
 
-    public EducationPlan(ArrayList<Activity> activities) {
+    public EducationPlan(ArrayList<Activity> activities, LocalDate expirationDate) {
         this.activities = activities;
+        this.expirationDate = expirationDate;
     }
 
-    public Knowledge apply(Student student) {
-        for (Activity activity : activities) {
-            activity.teach(student);
+    public Knowledge apply(Student student, LocalDate currentDate) {
+        while (currentDate.isBefore(expirationDate) || currentDate.equals(expirationDate)) {
+            for (Activity activity : activities) {
+                activity.teach(student);
+            }
+            currentDate = currentDate.plusDays(1);
         }
         return student.getKnowledge();
     }
--- test/EducationPlanManagerTest.java
+++ test/EducationPlanManagerTest.java
@@ -2,6 +2,7 @@ import educationPlanManager.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,17 +14,21 @@ class EducationPlanManagerTest {
     private EducationPlan plan;
     private ArrayList<Activity> activities;
 
+    private LocalDate currentDate;
+
     @BeforeEach
     void setUp() {
+        currentDate = LocalDate.of(2019, 3, 5);
+
         student = new Student(1, new Knowledge(0, 0));
         activities = new ArrayList<>();
         activities.add(new SelfEducationStrategy());
-        plan = new EducationPlan(activities);
+        plan = new EducationPlan(activities, currentDate);
     }
 
     @Test
     void applyPlan__oneActivity__oneDay() {
-        Knowledge result = plan.apply(student);
+        Knowledge result = plan.apply(student, currentDate);
         assertThat(result.getPractical(), is(1.0));
         assertThat(result.getTheoretical(), is(1.0));
     }
@@ -31,7 +36,7 @@ class EducationPlanManagerTest {
     @Test
     void applyPlan__studentLearningCoefficient() {
         student = new Student(0.5, new Knowledge(0, 0));
-        Knowledge result = plan.apply(student);
+        Knowledge result = plan.apply(student, currentDate);
         assertThat(result.getPractical(), is(0.5));
         assertThat(result.getTheoretical(), is(0.5));
     }
@@ -41,12 +46,22 @@ class EducationPlanManagerTest {
         activities.add(new InternshipStrategy());
         activities.add(new UniversityStrategy());
         activities.add(new MeetUpStrategy());
-        plan = new EducationPlan(activities);
+        plan = new EducationPlan(activities, currentDate);
 
-        Knowledge result = plan.apply(student);
+        Knowledge result = plan.apply(student, currentDate);
         assertThat(result.getPractical(), is(6.5));
         assertThat(result.getTheoretical(), is(6.5));
     }
 
+    @Test
+    void applyPlan__manyActivities__threeDays() {
+        activities.add(new InternshipStrategy());
+        activities.add(new UniversityStrategy());
+        activities.add(new MeetUpStrategy());
+        plan = new EducationPlan(activities, currentDate.plusDays(2));
 
+        Knowledge result = plan.apply(student, currentDate);
+        assertThat(result.getPractical(), is(19.5));
+        assertThat(result.getTheoretical(), is(19.5));
+    }
 }
